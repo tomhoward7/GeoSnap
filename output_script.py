465a8ea981350baefb792d85db1325ef42585b25
Tom Howard <tomhoward7@me.com>
2020-09-21 19:50:43 +0100

    Minor changes

--- output_script.py
+++ output_script.py
@@ -1,9 +1,11 @@
 from wikipedia_summary import landmark_summary
 from landmark_detect import landmark_description
-from landmark_detect import landmark_coordinates
+from landmark_detect import landmark_lat
+from landmark_detect import landmark_long
 
 print('Location:')
 print(landmark_description)
 print('Coordinates:')
-print(landmark_coordinates)
+print(landmark_lat)
+print(landmark_long)
 print(landmark_summary)
