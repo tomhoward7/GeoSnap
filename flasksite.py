df0a5f417a38231761d939d2a583652dd29328db
Tom Howard <tomhoward7@me.com>
2020-09-21 17:03:32 +0100

    Add Google Maps app to Flask

--- flasksite.py
+++ flasksite.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import FileField
-from flask_uploads import configure_uploads, IMAGES, UploadSet    
+from flask_uploads import configure_uploads, IMAGES, UploadSet
+from flask_googlemaps import GoogleMaps  
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'thisisasecret'
 app.config['UPLOADED_IMAGES_DEST'] = 'uploads/images'
+app.config['GOOGLEMAPS_KEY'] = "8JZ7i18MjFuM35dJHq70n3Hx4"
+GoogleMaps(app)
 
 #@app.route("/")
 #def home():
@@ -43,3 +46,37 @@ def index():
         return f'Filename: { filename }'
 
     return render_template('index.html', form=form)
+
+
+@app.route("/map")
+def mapview():
+    # creating a map in the view
+    mymap = Map(
+        identifier="view-side",
+        lat=37.4419,
+        lng=-122.1419,
+        markers=[(37.4419, -122.1419)]
+    )
+    sndmap = Map(
+        identifier="sndmap",
+        lat=37.4419,
+        lng=-122.1419,
+        markers=[
+          {
+             'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+             'lat': 37.4419,
+             'lng': -122.1419,
+             'infobox': "<b>Hello World</b>"
+          },
+          {
+             'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+             'lat': 37.4300,
+             'lng': -122.1400,
+             'infobox': "<b>Hello World from other place</b>"
+          }
+        ]
+    )
+    return render_template('example.html', mymap=mymap, sndmap=sndmap)
+
+if __name__ == "__main__":
+    app.run(debug=True)
