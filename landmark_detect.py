04f86dd40bdc44a944e9573a3374a04e1cf593b4
Tom Howard <tomhoward7@me.com>
2020-10-03 17:48:39 +0100

    Fix landmark detection script

--- landmark_detect.py
+++ landmark_detect.py
@@ -2,9 +2,10 @@ import io
 import os
 
 from google.cloud import vision
-from google.cloud.vision import types
+#from google.cloud.vision import types
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'ServiceAccountToken.json'
+
 
 client = vision.ImageAnnotatorClient()
 
@@ -16,18 +17,20 @@ image_path = f'./uploads/images/{file_name}'
 with io.open(image_path, 'rb') as image_file:
     content = image_file.read()
 
-image = vision.types.Image(content=content)
+#image = vision.types.Image(content=content)
+image = vision.Image(content=content)
 response = client.landmark_detection(image=image)
-landmarks = response.landmark_annotations
+landmark = response.landmark_annotations[0]
 
 #print('Location:')
-for landmark in landmarks:
-    #print("Location found!")
-    #print(landmark.description)
-    for location in landmark.locations:
-        lat_lng = location.lat_lng
-    #   print('Latitude {}'.format(lat_lng.latitude))
-    #   print('Longitude {}'.format(lat_lng.longitude))
+##for landmark in landmarks:
+#print("Location found!")
+#print(landmark.description)
+#print(landmark.score)
+for location in landmark.locations:
+    lat_lng = location.lat_lng
+#    print('Latitude {}'.format(lat_lng.latitude))
+#    print('Longitude {}'.format(lat_lng.longitude))
 
 if response.error.message:
     raise Exception('Could not determin the location of the photo.')
